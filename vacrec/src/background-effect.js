a1b94d43671500985c3d8bba6b062b4e7dae4e65
Praweenporn <71056576+Praweenporn@users.noreply.github.com>
2022-12-22 17:30:13 +0700

    create navbar

---
+++ vacrec/src/background-effect.js
@@ -0,0 +1,11 @@
+import bg from './img/Cover Page 3-01.jpg';
+
+function Background() {
+    return (
+        <div className="bg">
+            
+        </div>
+    );
+}
+
+export default Background;
