0da0821f73c07e80a54972ab2f376345d3dea78b
Praweenporn <71056576+Praweenporn@users.noreply.github.com>
2022-12-23 15:08:21 +0700

    create style of iframe

--- vacrecapp/src/background-effect.js
+++ vacrecapp/src/background-effect.js
@@ -28,47 +28,6 @@ function Background() {
                 <br></br>
                 <p>
                     <iframe className='datatable' src="./datatables.html"></iframe>
-              {/*   <table id="example" class="table table-striped table-bordered">
-                <thead>
-                <tr>
-                    <th>Company</th>
-                    <th>Contact</th>
-                    <th>Country</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>Alfreds Futterkiste</td>
-                    <td>Maria Anders</td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td>Centro comercial Moctezuma</td>
-                    <td>Francisco Chang</td>
-                    <td>Mexico</td>
-                </tr>
-                <tr>
-                    <td>Ernst Handel</td>
-                    <td>Roland Mendel</td>
-                    <td>Austria</td>
-                </tr>
-                <tr>
-                    <td>Island Trading</td>
-                    <td>Helen Bennett</td>
-                    <td>UK</td>
-                </tr>
-                <tr>
-                    <td>Laughing Bacchus Winecellars</td>
-                    <td>Yoshi Tannamuri</td>
-                    <td>Canada</td>
-                </tr>
-                <tr>
-                    <td>Magazzini Alimentari Riuniti</td>
-                    <td>Giovanni Rovelli</td>
-                    <td>Italy</td>
-                </tr>
-                </tbody>
-                </table>  */}
                 </p>
                 
                 <br></br>
