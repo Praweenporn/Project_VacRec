d5ce7fea6f1f5c48c4695241e0a2253dffc66398
Praweenporn <71056576+Praweenporn@users.noreply.github.com>
2022-12-23 09:30:51 +0700

    home page

--- vacrecapp/src/background-effect.js
+++ vacrecapp/src/background-effect.js
@@ -1,20 +1,32 @@
 import bg from './img/CoverPage.jpg';
+import view from './img/analysis.png'
 import './css/bg-effect.css'
 
 function Background() {
     return (
+        <div>
         <div className="container">
-                {/*      <div className="header"> */}
-                <img src={bg} alt="bg" className='headerBg' id='headerBg'/>
+              
+            <img src={bg} alt="bg" className='headerBg' id='headerBg'/>
             
             <div className="positiontext">
             <h1 className='texttitle' id='text'>Vaccine Patient Record</h1>
-            <section>
-            <p>
-               
-               </p>
-            </section>
+
+            <div className='topBtn'>
+            <button className='btnV'><span className='astext'>View</span><img className='viewT' src={view} /></button>
             </div>
+            
+            {/* <img className='viewT' src={view} /> */}
+           </div>
+
+            </div>
+
+            <div className='bgBlue'>
+                    <div className="textP">ข้อมูลผู้ป่วย</div>
+
+        
+            </div>
+
             </div>
     );
     }
